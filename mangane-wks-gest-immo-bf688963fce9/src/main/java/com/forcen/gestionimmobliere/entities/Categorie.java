8fc256e5c910a24c855a2d27e2077d891aadeae1
BaldeSalif <salifbalde2018@gmail.com>
2023-10-15 12:38:33 +0000

    modification des entités

--- mangane-wks-gest-immo-bf688963fce9/src/main/java/com/forcen/gestionimmobliere/entities/Categorie.java
+++ mangane-wks-gest-immo-bf688963fce9/src/main/java/com/forcen/gestionimmobliere/entities/Categorie.java
@@ -1,24 +1,2 @@
-package com.forcen.gestionimmobliere.entities;
-
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.io.Serializable;
-import java.util.List;
-
-
-@Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class Categorie  implements Serializable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String libelle;
-
-    @OneToMany(mappedBy = "categorie", fetch = FetchType.LAZY)
-    List<Bien> biens;
+package com.forcen.gestionimmobliere.entities;public class Categorie {
 }
