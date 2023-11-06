f14ab94d8b6adfb1cb605ae5f3e84b68bbd598b1
Zanika99 <fane.anne@unchk.edu.sn>
2023-11-06 13:32:51 +0000

    ancienne

--- mangane-wks-gest-immo-bf688963fce9/src/main/java/com/forcen/gestionimmobliere/entities/Categorie.java
+++ mangane-wks-gest-immo-bf688963fce9/src/main/java/com/forcen/gestionimmobliere/entities/Categorie.java
@@ -1,2 +1,21 @@
-package com.forcen.gestionimmobliere.entities;public class Categorie {
+package com.forcen.gestionimmobliere.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Categorie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String libelle;
+    @OneToMany(mappedBy = "categorie",fetch = FetchType.LAZY)
+    private List<Bien> biens;
 }
