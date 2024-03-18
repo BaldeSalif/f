a05e078fa4c5215d5d5670bdfb5499c8858193b4
Laye <73196482+FortLaye@users.noreply.github.com>
2024-03-18 22:34:12 +0000

    Update CategorieDTO.java
    Correction

--- mangane-wks-gest-immo-bf688963fce9/src/main/java/com/forcen/gestionimmobliere/web/dtos/CategorieDTO.java
+++ mangane-wks-gest-immo-bf688963fce9/src/main/java/com/forcen/gestionimmobliere/web/dtos/CategorieDTO.java
@@ -4,6 +4,7 @@ import com.forcen.gestionimmobliere.services.impl.CategorieServiceImpl;
 
 public record CategorieDTO(Long id, String libelle) {
 
+    // Supprime ce bloc de code ligne 8 à 11, il y a rien à écrire ici, laisses les accolates vides
     @SuppressWarnings("null")
     public void deleteCategorie(CategorieServiceImpl categorieServiceImpl) {
         categorieServiceImpl.categorieRepository.deleteById(id());
